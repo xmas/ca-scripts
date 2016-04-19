bd1bce0574e3dfda555a5481fc3c722b11eda685
Rowan Christmas <rowanxmas@gmail.com>
2016-04-18 21:45:42 -0700

    fb

--- server.js
+++ server.js
@@ -22,17 +22,16 @@ app.get('/webhook/', function (req, res) {
 })
 
 app.post('/webhook/', function (req, res) {
-       console.log('GOT REQUEST: '+req);
-  // messaging_events = req.body.entry[0].messaging;
-  // for (i = 0; i < messaging_events.length; i++) {
-  //   event = req.body.entry[0].messaging[i];
-  //   sender = event.sender.id;
-  //   if (event.message && event.message.text) {
-  //     text = event.message.text;
-      //sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));    }
-     // sendTextMessage(sender, "Confirmation!");
+    console.log('GOT REQUEST: '+req);
 
-
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));    }
+  }
   res.sendStatus(200);
 });
 
